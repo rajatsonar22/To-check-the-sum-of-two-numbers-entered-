b7c85ce838aba6c4de1586d12f8cd09b34314375
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2023-12-10 13:42:18 +0530

    Add files via upload

---
+++ to check the sum of two nmbers.py
@@ -0,0 +1,14 @@
+#to print the the sum  of two numbeers using python
+n2=int(input("Enter a number"))
+for i in range (n2+1):
+    print (i)
+
+n4=int(input("Enter a number"))
+sum=0
+i=0
+while (i<n4):
+    sum=sum+i
+    i=i+1
+    print(sum)
+    
+
